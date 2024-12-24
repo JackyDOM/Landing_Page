9c2c4f1d6d1dc0b08a486bfda8cf44288aab939d
Vichea <sokhomsovichea@gmail.com>
2024-12-24 15:14:36 +0700

    update icon

--- src/utils/iconMapping.ts
+++ src/utils/iconMapping.ts
@@ -1,12 +1,12 @@
 import { PiMapPinFill, PiPhoneCallFill, PiUsersFour, PiUsersThree } from "react-icons/pi";
 import { IoMailSharp } from 'react-icons/io5';
 import { RxHome } from "react-icons/rx";
-import { FiAnchor } from "react-icons/fi";
 import { BsDatabaseCheck } from "react-icons/bs";
 import { RiUserStarLine } from "react-icons/ri";
 import { AiOutlineMessage } from "react-icons/ai";
 import { LiaHandsSolid } from "react-icons/lia";
 import { MdOutlineContactPage } from "react-icons/md";
+import { MdOndemandVideo } from "react-icons/md";
 
 export const contactIconMapping: any = {
   PiPhoneCallFill: PiPhoneCallFill,
@@ -16,7 +16,7 @@ export const contactIconMapping: any = {
 
 export const menuIconMapping: any = {
   RxHome: RxHome,
-  FiAnchor: FiAnchor,
+  MdOndemandVideo: MdOndemandVideo,
   BsDatabaseCheck: BsDatabaseCheck,
   LiaHandsSolid: LiaHandsSolid,
   PiUsersThree: PiUsersThree,
